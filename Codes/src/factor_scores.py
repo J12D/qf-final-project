7011c7893da6f2a6dbd4da664d346ac3df1cf533
AlexVuLe <vu.le.williams@gmail.com>
2012-11-21 23:21:14 -0500

    clean up data and convert to timeseries

--- Codes/src/factor_scores.py
+++ Codes/src/factor_scores.py
@@ -7,14 +7,17 @@ import pandas as pd
 import matplotlib.pylab as plt
 import statsmodels.api as sm
 import numpy as np
+from datetime import datetime as dt
 import os
-ws = os.path.expanduser('~/qf-final-project/')
+ws = os.path.expanduser('~/Documents/workspace/qf-final-project/')
 os.chdir(ws)
 
 #===============================================================================
 # Clean up data
 #===============================================================================
-fx_data = pd.read_csv('data/full.csv', sep = '\t')
-fx_data.GOVT_YIELD_10YR = fx_data.GOVT_YIELD_10YR.apply(lambda x: x[:-2])
-fx_data.ix[:,2:6] = fx_data.ix[:,2:6].replace('\\N', np.NaN).apply(np.float64)
- 
+fx_data = pd.read_csv('data/full.csv')
+fx_data.ix[:,2:] = fx_data.ix[:,2:].replace('\\N', np.NaN).apply(np.float64)
+fx_data.Date = fx_data.Date.map(lambda x: x[:10])
+fx_data.Date = fx_data.Date.map(lambda x: dt.strptime(x, '%Y-%m-%d'))
+fx_data.index = fx_data.Date
+currency = fx_data.groupby('Currency')

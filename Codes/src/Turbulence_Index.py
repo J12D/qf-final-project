ac0f973f1072ec784f503d126109df0b3139413e
AlexVuLe <vu.le.williams@gmail.com>
2012-11-25 21:03:15 -0500

    Computed signals using Turbulence index

--- Codes/src/Turbulence_Index.py
+++ Codes/src/Turbulence_Index.py
@@ -43,30 +43,29 @@ TI = TI.dropna()
 index = TI.index
 TI.index = range(len(TI))
 Turbulence = pd.DataFrame(index = TI.index)
-Turbulence['values'] = np.NaN
+Turbulence['TI'] = np.NaN
 
 for i in xrange(20,len(TI)):
     mean = TI.ix[:i-1].mean().values
     cov = np.matrix(np.cov(TI.ix[:i-1].T))
     rets = TI.ix[i].values
     d = rets - mean 
-    Turbulence.ix[i,'values'] = d.dot(cov.I).dot(d.T)
+    Turbulence.ix[i,'TI'] = d.dot(cov.I).dot(d.T)
     
 Turbulence.index = index
 Turbulence = Turbulence.dropna()
 
 from momentum import efa
-Turbulence['MA'] = efa(Turbulence.values,1,20)
+Turbulence['MA'] = efa(Turbulence.TI,1,30)
 Turbulence.MA.ix[:29] = np.NaN
 
 def get_weight(interval, q, method = 'continuous'):
     if not q:
         return np.NaN
-    pc = np.percentile(range, q)
-    print pc
+    pc = sum(interval <= q)/np.float64(len(interval))
     if method == 'continuous':
         if pc <= 0.2:
-            return 1
+            return 1.0
         elif pc <= 0.4:
             return 0.8
         elif pc <= 0.6:
@@ -75,13 +74,22 @@ def get_weight(interval, q, method = 'continuous'):
             return 0.4
         else:
             return 0.2
-    else:
+    elif method == 'discrete':
         if pc <= 0.8:
             return 1
         else:
             return 0
+    else:
+        raise ValueError('Unrecognized method')
 
-get_weight(Turbulence.values, 800)
-      
+Turbulence['cont_weight'] = np.NaN
+Turbulence['discrete_weight'] = np.NaN
+for i in xrange(3*365+1,len(Turbulence)):
+    Turbulence.ix[i,'cont_weight'] =\
+    get_weight(Turbulence.TI[(i-3*365+1):(i-1)], Turbulence.TI[i-1])
+    Turbulence.ix[i,'discrete_weight'] =\
+    get_weight(Turbulence.TI[(i-3*365+1):(i-1)], Turbulence.TI[i-1], method = 'discrete')
+    
+         
     
     

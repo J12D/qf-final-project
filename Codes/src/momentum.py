09ab9bcaf103ea74eca99f47acccb1f902a1ea7c
Julian Debus <heinzderalligator@googlemail.com>
2012-11-27 21:23:55 -0500

    Problematic momentum: Breaks for some currencies

--- Codes/src/momentum.py
+++ Codes/src/momentum.py
@@ -6,12 +6,21 @@ import foundation as fd
 from functions import efa
 
 # will change implementation later -> getFxRates()
-fxData=fd.getFxRatesOLD()
+#fxData=fd.getFxRatesOLD()
 
+fxData=fd.getFxRates()
+fxData.set_index('Date')
+fxData=fxData[fxData.Currency!='USD']
+fxData=fxData.groupby('Currency').apply(lambda x: x.ix[:,1:].fillna(method = 'ffill',limit = 30 ))
+#This breaks
+#fxData.groupby('Currency').SPOT.apply(lambda x: efa(x,0.97,260))
 
-efa(fxData,0.97,260)
+#This works
+efa(fxData[fxData.Currency=='AUD']['SPOT'],0.97,300)
 
-ema = data.copy()
+#This doesn't
+efa(fxData[fxData.Currency=='CAD']['SPOT'],0.97,300)
+#ema = data.copy()
 # You can' use print for this
-data.apply(lambda x:print x,axis=0)
-emadata=data.apply(lambda x:efa(x,0.97,260),axis=0)
+#data.apply(lambda x:print x,axis=0)
+#emadata=data.apply(lambda x:efa(x,0.97,260),axis=0)

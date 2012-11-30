e0abf529169e128cc39af06e2a6997504f8b3e22
AlexVuLe <vu.le.williams@gmail.com>
2012-11-30 05:18:52 -0500

    move value.py to factor_scores.py, finished computing and evaluating PPP --- need to double check formula

--- Codes/src/factor_scores.py
+++ Codes/src/factor_scores.py
@@ -14,10 +14,12 @@ foreign = foreign.groupby('Currency').apply(lambda x: x.ix[:,1:].fillna(method =
 # Compute monthly returns for each currency
 monthly_rets = lambda x: x.SPOT/x.FRWD_1M.shift() - 1
 foreign['rets'] = foreign[['SPOT','FRWD_1M']].groupby(level='Currency').apply(monthly_rets).values
+
+# Compute mom factors
 foreign['mom_12'] = foreign.SPOT.groupby(level='Currency').pct_change().values 
 foreign['mom_26'] = foreign.SPOT.groupby(level='Currency').pct_change(periods = 4).values
 
-# Compute and test carry factor
+# Compute and test carry and mom factor
 foreign['carry'] = -(foreign.FRWD_1M/foreign.SPOT - 1)
 foreign.carry = foreign.carry.groupby(level = 'Currency').shift()
 foreign.mom_12 = foreign.mom_12.groupby(level = 'Currency').shift()
@@ -35,3 +37,28 @@ mom12_betas = foreign[['mom_12','rets']].groupby(level = 1).apply(monthly_reg)
 eval_factor(mom12_betas)
 mom26_betas = foreign[['mom_26','rets']].groupby(level = 1).apply(monthly_reg)
 eval_factor(mom26_betas)
+
+# Compute PPP factors
+cpi = fd.get_monthly_CPI()
+foreign_CPI = cpi.ix['AUD' : 'SEK']
+US_CPI = cpi.ix['USD'].CPI
+US_CPI.name = 'US_CPI'
+US_CPI = US_CPI.reset_index() 
+US_CPI = US_CPI.rename(columns = {'index': 'Date'})
+
+lag = 3
+
+def PPP(df):
+    us = US_CPI.copy()
+    us['Currency'] = df.index[0][0]
+    us = us.set_index(['Currency','Date'])
+    return df.SPOT*df.CPI/us.US_CPI
+
+foreign_CPI['PPP'] = foreign_CPI.groupby(level='Currency').apply(PPP).values
+foreign = foreign.join(foreign_CPI.PPP)
+foreign.PPP = foreign.PPP.groupby(level = 'Currency').shift(periods = lag)
+PPP_betas = foreign[['PPP','rets']].groupby(level = 1).apply(monthly_reg)
+eval_factor(PPP_betas)
+
+((1+carry_betas/100).cumprod()-1).plot()
+plt.show()
--- Codes/src/foundation.py
+++ Codes/src/foundation.py
@@ -13,23 +13,23 @@ def getRootDir():
 	return root
 
 def getFxRatesOLD():
-    #import fxSpotRates
-    fxfile=os.path.join(getRootDir(),'data/qf-fxSpotRates.csv')
-    data=pd.read_csv(fxfile)
-    #convert index into date object
-    for i in range(len(data.Index)):
-        data.Index[i]=dt.datetime.strptime(data.Index[i],"%Y-%m-%d")
-    return data
+	#import fxSpotRates
+	fxfile=os.path.join(getRootDir(),'data/qf-fxSpotRates.csv')
+	data=pd.read_csv(fxfile)
+	#convert index into date object
+	for i in range(len(data.Index)):
+		data.Index[i]=dt.datetime.strptime(data.Index[i],"%Y-%m-%d")
+	return data
 
 # Should only be called once in the beginning
 def getFxRates():
-    os.chdir(getRootDir())
-    fx_data = pd.read_csv('data/full.csv')
-    fx_data.ix[:,2:] = fx_data.ix[:,2:].replace('\\N', np.NaN).apply(np.float64)
-    fx_data.Date = fx_data.Date.map(lambda x: x[:10])
-    fx_data.Date = fx_data.Date.map(lambda x: dt.strptime(x, '%Y-%m-%d'))
-    fx_data = fx_data.set_index('Date')
-    return fx_data
+	os.chdir(getRootDir())
+	fx_data = pd.read_csv('data/full.csv')
+	fx_data.ix[:,2:] = fx_data.ix[:,2:].replace('\\N', np.NaN).apply(np.float64)
+	fx_data.Date = fx_data.Date.map(lambda x: x[:10])
+	fx_data.Date = fx_data.Date.map(lambda x: dt.strptime(x, '%Y-%m-%d'))
+	fx_data = fx_data.set_index('Date')
+	return fx_data
 
 #uncomment this to test wether the code is working correctly
 #print "The root of the project is: "+getRootDir()
@@ -42,7 +42,7 @@ def get_monthly_CPI():
 	cpi.Date = cpi.Date.map(lambda x: dt.strptime(x, '%Y-%m-%d'))
 	cpi = cpi.set_index('Date')
 	cpi = cpi.groupby('Currency').asfreq('BM', method = 'bfill')
-	cpi = cpi[['CPI','Currency']]
+	cpi = cpi['CPI']
 	
 	fx_data = pd.read_csv('data/full.csv')
 	fx_data = fx_data[['Date', 'Currency', 'SPOT']]
